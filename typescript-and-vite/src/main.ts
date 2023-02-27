12ddbe06d53ce97ed84b1f5a1000bd1e8bb4048f
calimendoza <mbaivictor1@gmail.com>
2023-02-27 02:17:13 -0800

    sveTasks fucntion

--- typescript-and-vite/src/main.ts
+++ typescript-and-vite/src/main.ts
@@ -10,25 +10,43 @@ type Task = {
 const list = document.querySelector<HTMLUListElement>('#list')
 const input = document.querySelector<HTMLInputElement>('#new-task-title')
 const form = document.getElementById('#new-task-form') as HTMLFormElement
+const tasks: Task[] = []
 
 
 form.addEventListener("submit", e => {
    e.preventDefault()
-
    if(input?.value == "" ||  input?.value == null) return
    
-   const newTask = {
-       
-    id: uuidV4(),
-    title: input.value,
-    completed: false,
-    createdAt: new Date(),
+   const newTask: Task = {
+   id: uuidV4(),
+   title: input.value,
+   completed: false,
+   createdAt: new Date(),
 
    }
-
+   tasks.push(newTask)
+   saveTasks()
    addListItem(newTask)
+   input.value = ''
 })
 
 function addListItem(task: Task){
+   const item = document.createElement("li")
+   const label = document.createElement("label")
+   const checkbox = document.createElement("input")
 
+   checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+      saveTasks()
+   })
+   checkbox.type = "checkbox"
+   checkbox.checked = checkbox.checked
+
+   label.append(checkbox, task.title)
+   item.append(label)
+   list?.append(item)
+}
+
+function saveTasks() {
+  
 }
